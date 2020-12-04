77d6ac47da9fe5ade2827dcf59616c224ffcd536
Andy <andy.bonnetto@epfl.ch>
2020-12-04 14:24:14 +0100

    test picam

--- Carderly_Vision/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/Activate_Vision.py
+++ Carderly_Vision/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/Activate_Vision.py
@@ -197,12 +197,12 @@ while not some_condition:
         if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1, (boxes[i][0] * imH)))
-            xmin = int(max(1, (boxes[i][1] * imW)))
-            ymax = int(min(imH, (boxes[i][2] * imH)))
-            xmax = int(min(imW, (boxes[i][3] * imW)))
-
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+            # ymin = int(max(1, (boxes[i][0] * imH)))
+            # xmin = int(max(1, (boxes[i][1] * imW)))
+            # ymax = int(min(imH, (boxes[i][2] * imH)))
+            # xmax = int(min(imW, (boxes[i][3] * imW)))
+            #
+            # cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
 
             # Draw label
             object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
